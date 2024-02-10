6373601f8be9682163df58a6f521cd01e0f257b7
Raushan <roushankumarsingh2018@gmail.com>
2024-02-11 00:24:36 +0530

    added aboutus and developer routes

---
+++ pages/aboutus/developer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const developer = () => {
+  return (
+    <h1>
+      this is developer page
+    </h1>
+  )
+}
+
+export default developer
---
+++ pages/news/index.js
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+const NewsPage = () => {
+    return (
+      <h1>
+        NewsPage
+      </h1>
+    )
+  }
+  
+  export default NewsPage
+  
